a010babb16e9374c42a3b64a1565cecbae24245b
Jonathan Roemer <jroemer@datto.com>
2016-05-17 18:45:13 -0400

    It isn't clean, but it works.

--- test-sec-flags.sh
+++ test-sec-flags.sh
@@ -13,7 +13,20 @@ fi
 
 cd "$localdir"/UnixBench
 
-echo -e "Compiling with -fstack-protector-string and partial relro" 
+echo -e "Compiling with -fstack-protector-strong and partial relro" 
 sed -i '/^CFLAGS = -Wall -pedantic/c\CFLAGS = -z,relro -Wall -pedantic $(OPTON) -I $(SRCDIR) -DTIME -fstack-protector-strong -D_FORTIFY_SOURCE=2' Makefile
-make
-#./Run 2>&1 tee testresults-fstack-relropartial.txt
+make || exit
+# result destination folders are hardcoded in Run. We should sed in new paths 
+# instead of doing this nonsense
+echo -e "Compilation finished, running test 1"
+bash -c './Run' &> testresults-fstack-relropartial.txt
+echo -e "Test 1 completed."
+make clean
+
+echo -e "Compiling with -fstack-protector-strong, partial relro, and -fstack-check"
+sed -i '/^CFLAGS = -z,relro/c\CFLAGS = -z,relro -Wall -pedantic $(OPTON) -I $(SRCDIR) -DTIME -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check' Makefile
+make || exit
+echo -e "Compilation finished, running test 2"
+bash -c './Run' &> testresults-fstack-relropartial-fstackcheck.txt
+echo -e "Test 2 completed."
+make clean
