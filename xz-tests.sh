489316122073b3ffb7c5cd396807d41042d0815c
Jonathan Roemer <jroemer@datto.com>
2016-05-22 12:11:22 -0400

    Basic implementation should be completed

--- xz-tests.sh
+++ xz-tests.sh
@@ -24,7 +24,19 @@ fi
 cd "$localdir"
 echo -e "running ./configure"
 ./configure
-cd src/xz
+
+git checkout src/xz/Makefile
+sed 's/^CFLAGS = -g -O2/CFLAGS += -g -O2/g' -i Makefile
+
+# download something to compress
+if [ ! -f 'linux-4.6.tar' ]
+    then
+        echo -e "Downloading linux source files..."
+        curl -O 'https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.6.tar.xz'
+        xz -d 'linux-4.6.tar.xz'
+    else
+        echo -e "linux source files already downloaded. Continuing..."
+fi
 
 echo -e "Compiling with -fstack-protector-strong and partial relro"
 make clean
@@ -32,7 +44,7 @@ CFLAGS='-Wl,-z,relro -fstack-protector-strong -D_FORTIFY_SOURCE=2' make
 # result destination folders are hardcoded in Run. We should sed in new paths
 # instead of doing this nonsense
 echo -e "Compilation finished, running test 1"
-time  |& tee test1.txt
+time xz -ke 'linux-4.6.tar' |& tee test1.txt
 echo -e "Test 1 completed."
 
 
@@ -40,7 +52,7 @@ echo -e "Compiling with -fstack-protector-strong, partial relro, and -fstack-che
 make clean
 CFLAGS='-Wl,-z,relro -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check' make
 echo -e "Compilation finished, running test 2"
-./ffmpeg -benchmark -i input.mkv -f null - |& tee test2.txt
+time xz -ke 'linux-4.6.tar' |& tee test2.txt
 echo -e "Test 2 completed."
 
 
@@ -48,7 +60,7 @@ echo -e "Compiling with -fstack-protector-strong, partial relro, and PIE"
 make clean
 CFLAGS='-Wl,-z,relro -fstack-protector-strong -D_FORTIFY_SOURCE=2 -pie -fPIE' make
 echo -e "Compilation finished, running test 3"
-./ffmpeg -benchmark -i input.mkv -f null - |& tee test3.txt
+time xz -ke 'linux-4.6.tar' |& tee test3.txt
 echo -e "Test 3 completed."
 
 
@@ -56,7 +68,7 @@ echo -e "Compiling with -fstack-protector-strong, partial relro, PIE, and -fstac
 make clean
 CFLAGS='-Wl,-z,relro -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check -pie -fPIE' make
 echo -e "Compilation finished, running test 4"
-./ffmpeg -benchmark -i input.mkv -f null - |& tee test4.txt
+time xz -ke 'linux-4.6.tar' |& tee test4.txt
 echo -e "Test 4 completed."
 
 
@@ -64,7 +76,7 @@ echo -e "Compiling with -fstack-protector-strong, full relro, PIE"
 make clean
 CFLAGS='-Wl,-z,relro,-z,now -fstack-protector-strong -D_FORTIFY_SOURCE=2 -pie -fPIE' make
 echo -e "Compilation finished, running test 5"
-./ffmpeg -benchmark -i input.mkv -f null - |& tee test5.txt
+time xz -ke 'linux-4.6.tar' |& tee test5.txt
 echo -e "Test 5 completed."
 
 
@@ -72,7 +84,7 @@ echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fstack-check
 make clean
 CFLAGS='-Wl,-z,relro,-z,now -fstack-protector-strong -D_FORTIFY_SOURCE=2 -pie -fPIE -fstack-check' make
 echo -e "Compilation finished, running test 6"
-./ffmpeg -benchmark -i input.mkv -f null - |& tee test6.txt
+time xz -ke 'linux-4.6.tar' |& tee test6.txt
 echo -e "Test 6 completed."
 
 
@@ -80,7 +92,7 @@ echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fno-plt"
 make clean
 CFLAGS='-Wl,-z,relro,-z,now -fstack-protector-strong -D_FORTIFY_SOURCE=2 -pie -fPIE -fno-plt' make
 echo -e "Compilation finished, running test 7"
-./ffmpeg -benchmark -i input.mkv -f null - |& tee test7.txt
+time xz -ke 'linux-4.6.tar' |& tee test7.txt
 echo -e "Test 7 completed."
 
 
@@ -88,5 +100,5 @@ echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fno-plt, and
 make clean
 CFLAGS='-Wl,-z,relro,-z,now -fstack-protector-strong -D_FORTIFY_SOURCE=2 -pie -fPIE -fno-plt -fstack-check' make
 echo -e "Compilation finished, running test 8"
-./ffmpeg -benchmark -i input.mkv -f null - |& tee test8.txt
+time xz -ke 'linux-4.6.tar' |& tee test8.txt
 echo -e "Test 8 completed."
