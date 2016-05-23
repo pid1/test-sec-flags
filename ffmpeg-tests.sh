249948d7d52b73e628dbb694e7858416bdf567e5
anthraxx <levente@leventepolyak.net>
2016-05-23 17:58:51 +0200

    ffmpeg test suite fixes (wip)

--- ffmpeg-tests.sh
+++ ffmpeg-tests.sh
@@ -23,57 +23,58 @@ if [ ! -d "$localdir" ]
 fi
 
 cd "$localdir"
-echo -e "running ./configure"
 
-./configure \
-    --prefix='/usr' \
-    --disable-debug \
-    --disable-static \
-    --disable-stripping \
-    --enable-avisynth \
-    --enable-avresample \
-    --enable-fontconfig \
-    --enable-gnutls \
-    --enable-gpl \
-    --enable-ladspa \
-    --enable-libass \
-    --enable-libbluray \
-    --enable-libdcadec \
-    --enable-libfreetype \
-    --enable-libfribidi \
-    --enable-libgsm \
-    --enable-libiec61883 \
-    --enable-libmodplug \
-    --enable-libmp3lame \
-    --enable-libopencore_amrnb \
-    --enable-libopencore_amrwb \
-    --enable-libopenjpeg \
-    --enable-libopus \
-    --enable-libpulse \
-    --enable-libschroedinger \
-    --enable-libsoxr \
-    --enable-libspeex \
-    --enable-libssh \
-    --enable-libtheora \
-    --enable-libv4l2 \
-    --enable-libvidstab \
-    --enable-libvorbis \
-    --enable-libvpx \
-    --enable-libwebp \
-    --enable-libx264 \
-    --enable-libx265 \
-    --enable-libxvid \
-    --enable-netcdf \
-    --enable-shared \
-    --enable-version3 \
-    --enable-x11grab
+CONFIGURE_OPTIONS="--prefix='/usr'
+    --disable-debug
+    --disable-static
+    --disable-stripping
+    --enable-avisynth
+    --enable-avresample
+    --enable-fontconfig
+    --enable-gnutls
+    --enable-gpl
+    --enable-ladspa
+    --enable-libass
+    --enable-libbluray
+    --enable-libdcadec
+    --enable-libfreetype
+    --enable-libfribidi
+    --enable-libgsm
+    --enable-libiec61883
+    --enable-libmodplug
+    --enable-libmp3lame
+    --enable-libopencore_amrnb
+    --enable-libopencore_amrwb
+    --enable-libopenjpeg
+    --enable-libopus
+    --enable-libpulse
+    --enable-libschroedinger
+    --enable-libsoxr
+    --enable-libspeex
+    --enable-libssh
+    --enable-libtheora
+    --enable-libv4l2
+    --enable-libvidstab
+    --enable-libvorbis
+    --enable-libvpx
+    --enable-libwebp
+    --enable-libx264
+    --enable-libx265
+    --enable-libxvid
+    --enable-netcdf
+    --enable-shared
+    --enable-version3
+    --enable-x11grab"
 
 if [ -z "$1" ] || [ "1" == "$1" ]; then
 	echo -e "Compiling with -fstack-protector-strong and partial relro"
+	LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro' \
+		CFLAGS='-fstack-protector-strong -pipe -O2' \
+		CXXFLAGS='-fstack-protector-strong -pipe -O2' \
+		CPPFLAGS='-D_FORTIFY_SOURCE=2' \
+		./configure ${CONFIGURE_OPTIONS}
 	make clean
-	LDFLAGS='-Wl,-z,relro' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2' make -j${JOBS}
-	# result destination folders are hardcoded in Run. We should sed in new paths
-	# instead of doing this nonsense
+	make -j${JOBS}
 	echo -e "Compilation finished, running test 1"
 	./ffmpeg -benchmark -i input.mkv -f null - |& tee test1.txt
 	echo -e "Test 1 completed."
@@ -82,8 +83,13 @@ fi
 
 if [ -z "$1" ] || [ "2" == "$1" ]; then
 	echo -e "Compiling with -fstack-protector-strong, partial relro, and -fstack-check"
+	LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro' \
+		CFLAGS='-fstack-protector-strong -fstack-check -pipe -O2' \
+		CXXFLAGS='-fstack-protector-strong -fstack-check -pipe -O2' \
+		CPPFLAGS='-D_FORTIFY_SOURCE=2' \
+		./configure ${CONFIGURE_OPTIONS}
 	make clean
-	LDFLAGS='-Wl,-z,relro' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check' make -j${JOBS}
+	make -j${JOBS}
 	echo -e "Compilation finished, running test 2"
 	./ffmpeg -benchmark -i input.mkv -f null - |& tee test2.txt
 	echo -e "Test 2 completed."
@@ -92,8 +98,13 @@ fi
 
 if [ -z "$1" ] || [ "3" == "$1" ]; then
 	echo -e "Compiling with -fstack-protector-strong, partial relro, and PIE"
+	LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro -fPIE -pie' \
+		CFLAGS='-fstack-protector-strong -pipe -O2 -fPIE -pie' \
+		CXXFLAGS='-fstack-protector-strong -pipe -O2 -fPIE -pie' \
+		CPPFLAGS='-D_FORTIFY_SOURCE=2' \
+		./configure ${CONFIGURE_OPTIONS}
 	make clean
-	LDFLAGS='-Wl,-z,relro -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE' make -j${JOBS}
+	make -j${JOBS}
 	echo -e "Compilation finished, running test 3"
 	./ffmpeg -benchmark -i input.mkv -f null - |& tee test3.txt
 	echo -e "Test 3 completed."
@@ -102,8 +113,13 @@ fi
 
 if [ -z "$1" ] || [ "4" == "$1" ]; then
 	echo -e "Compiling with -fstack-protector-strong, partial relro, PIE, and -fstack-check"
+	LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro -fPIE -pie' \
+		CFLAGS='-fstack-protector-strong -fstack-check -fPIE -pie -pipe -O2' \
+		CXXFLAGS='-fstack-protector-strong -fstack-check -fPIE -pie -pipe -O2' \
+		CPPFLAGS='-D_FORTIFY_SOURCE=2' \
+		./configure ${CONFIGURE_OPTIONS}
 	make clean
-	LDFLAGS='-Wl,-z,relro -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check -fPIE' make -j${JOBS}
+	make -j${JOBS}
 	echo -e "Compilation finished, running test 4"
 	./ffmpeg -benchmark -i input.mkv -f null - |& tee test4.txt
 	echo -e "Test 4 completed."
@@ -112,8 +128,13 @@ fi
 
 if [ -z "$1" ] || [ "5" == "$1" ]; then
 	echo -e "Compiling with -fstack-protector-strong, full relro, PIE"
+	LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -fPIE -pie' \
+		CFLAGS='-fstack-protector-strong -fPIE -pie -pipe -O2' \
+		CXXFLAGS='-fstack-protector-strong -fPIE -pie -pipe -O2' \
+		CPPFLAGS='-D_FORTIFY_SOURCE=2' \
+		./configure ${CONFIGURE_OPTIONS}
 	make clean
-	LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE' make -j${JOBS}
+	make -j${JOBS}
 	echo -e "Compilation finished, running test 5"
 	./ffmpeg -benchmark -i input.mkv -f null - |& tee test5.txt
 	echo -e "Test 5 completed."
@@ -122,8 +143,13 @@ fi
 
 if [ -z "$1" ] || [ "6" == "$1" ]; then
 	echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fstack-check"
+	LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -fPIE -pie' \
+		CFLAGS='-fstack-protector-strong -fPIE -pie -fstack-check -pipe -O2' \
+		CXXFLAGS='-fstack-protector-strong -fPIE -pie -fstack-check -pipe -O2' \
+		CPPFLAGS='-D_FORTIFY_SOURCE=2' \
+		./configure ${CONFIGURE_OPTIONS}
 	make clean
-	LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fstack-check' make -j${JOBS}
+	make -j${JOBS}
 	echo -e "Compilation finished, running test 6"
 	./ffmpeg -benchmark -i input.mkv -f null - |& tee test6.txt
 	echo -e "Test 6 completed."
@@ -132,8 +158,13 @@ fi
 
 if [ -z "$1" ] || [ "7" == "$1" ]; then
 	echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fno-plt"
+	LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -fPIE -pie' \
+		CFLAGS='-fstack-protector-strong -fPIE -pie -fno-plt -pipe -O2' \
+		CXXFLAGS='-fstack-protector-strong -fPIE -pie -fno-plt -pipe -O2' \
+		CPPFLAGS='-D_FORTIFY_SOURCE=2' \
+		./configure ${CONFIGURE_OPTIONS}
 	make clean
-	LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fno-plt' make -j${JOBS}
+	make -j${JOBS}
 	echo -e "Compilation finished, running test 7"
 	./ffmpeg -benchmark -i input.mkv -f null - |& tee test7.txt
 	echo -e "Test 7 completed."
@@ -142,8 +173,13 @@ fi
 
 if [ -z "$1" ] || [ "8" == "$1" ]; then
 	echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fno-plt, and -fstack-check"
+	LDFLAGS='-Wl,-O1,--sort-common,--as-needed,-z,relro,-z,now -fPIE -pie' \
+		CFLAGS='-fstack-protector-strong -fPIE -pie -fno-plt -fstack-check -pipe -O2' \
+		CXXFLAGS='-fstack-protector-strong -fPIE -pie -fno-plt -fstack-check -pipe -O2' \
+		CPPFLAGS='-D_FORTIFY_SOURCE=2' \
+		./configure ${CONFIGURE_OPTIONS}
 	make clean
-	LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fno-plt -fstack-check' make -j${JOBS}
+	make -j${JOBS}
 	echo -e "Compilation finished, running test 8"
 	./ffmpeg -benchmark -i input.mkv -f null - |& tee test8.txt
 	echo -e "Test 8 completed."
