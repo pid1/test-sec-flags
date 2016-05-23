d9cd67d6ec84ac8dcf817b05561187b84c534a46
anthraxx <levente@leventepolyak.net>
2016-05-23 14:04:18 +0200

    adding options to test just a single case via argv[1] -> test-num

--- unixbench-tests.sh
+++ unixbench-tests.sh
@@ -29,67 +29,83 @@ cd "$localdir"/UnixBench
 git checkout Makefile
 sed 's/^CFLAGS =/CFLAGS +=/g' -i Makefile
 
-echo -e "Compiling with -fstack-protector-strong and partial relro"
-make clean
-LDFLAGS='-Wl,-z,relro' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2' make
-# result destination folders are hardcoded in Run. We should sed in new paths
-# instead of doing this nonsense
-echo -e "Compilation finished, running test 1"
-bash -c './Run' &>> $results
-echo -e "Test 1 completed."
+if [ -z "$1" ] || [ "1" = "$1" ]; then
+	echo -e "Compiling with -fstack-protector-strong and partial relro"
+	make clean
+	LDFLAGS='-Wl,-z,relro' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2' make
+	# result destination folders are hardcoded in Run. We should sed in new paths
+	# instead of doing this nonsense
+	echo -e "Compilation finished, running test 1"
+	bash -c './Run' &>> $results
+	echo -e "Test 1 completed."
+fi
 
 
-echo -e "Compiling with -fstack-protector-strong, partial relro, and -fstack-check"
-make clean
-LDFLAGS='-Wl,-z,relro' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check' make
-echo -e "Compilation finished, running test 2"
-bash -c './Run' &>> $results
-echo -e "Test 2 completed."
+if [ -z "$1" ] || [ "2" = "$1" ]; then
+	echo -e "Compiling with -fstack-protector-strong, partial relro, and -fstack-check"
+	make clean
+	LDFLAGS='-Wl,-z,relro' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check' make
+	echo -e "Compilation finished, running test 2"
+	bash -c './Run' &>> $results
+	echo -e "Test 2 completed."
+fi
 
 
-echo -e "Compiling with -fstack-protector-strong, partial relro, and PIE"
-make clean
-LDFLAGS='-Wl,-z,relro -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE' make
-echo -e "Compilation finished, running test 3"
-bash -c './Run' &>> $results
-echo -e "Test 3 completed."
+if [ -z "$1" ] || [ "3" = "$1" ]; then
+	echo -e "Compiling with -fstack-protector-strong, partial relro, and PIE"
+	make clean
+	LDFLAGS='-Wl,-z,relro -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE' make
+	echo -e "Compilation finished, running test 3"
+	bash -c './Run' &>> $results
+	echo -e "Test 3 completed."
+fi
 
 
-echo -e "Compiling with -fstack-protector-strong, partial relro, PIE, and -fstack-check"
-make clean
-LDFLAGS='-Wl,-z,relro -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check -fPIE' make
-echo -e "Compilation finished, running test 4"
-bash -c './Run' &>> $results
-echo -e "Test 4 completed."
+if [ -z "$1" ] || [ "4" = "$1" ]; then
+	echo -e "Compiling with -fstack-protector-strong, partial relro, PIE, and -fstack-check"
+	make clean
+	LDFLAGS='-Wl,-z,relro -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fstack-check -fPIE' make
+	echo -e "Compilation finished, running test 4"
+	bash -c './Run' &>> $results
+	echo -e "Test 4 completed."
+fi
 
 
-echo -e "Compiling with -fstack-protector-strong, full relro, PIE"
-make clean
-LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE' make
-echo -e "Compilation finished, running test 5"
-bash -c './Run' &>> $results
-echo -e "Test 5 completed."
+if [ -z "$1" ] || [ "5" = "$1" ]; then
+	echo -e "Compiling with -fstack-protector-strong, full relro, PIE"
+	make clean
+	LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE' make
+	echo -e "Compilation finished, running test 5"
+	bash -c './Run' &>> $results
+	echo -e "Test 5 completed."
+fi
 
 
-echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fstack-check"
-make clean
-LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fstack-check' make
-echo -e "Compilation finished, running test 6"
-bash -c './Run' &>> $results
-echo -e "Test 6 completed."
+if [ -z "$1" ] || [ "6" = "$1" ]; then
+	echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fstack-check"
+	make clean
+	LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fstack-check' make
+	echo -e "Compilation finished, running test 6"
+	bash -c './Run' &>> $results
+	echo -e "Test 6 completed."
+fi
 
 
-echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fno-plt"
-make clean
-LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fno-plt' make
-echo -e "Compilation finished, running test 7"
-bash -c './Run' &>> $results
-echo -e "Test 7 completed."
+if [ -z "$1" ] || [ "7" = "$1" ]; then
+	echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fno-plt"
+	make clean
+	LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fno-plt' make
+	echo -e "Compilation finished, running test 7"
+	bash -c './Run' &>> $results
+	echo -e "Test 7 completed."
+fi
 
 
-echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fno-plt, and -fstack-check"
-make clean
-LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fno-plt -fstack-check' make
-echo -e "Compilation finished, running test 8"
-bash -c './Run' &>> $results
-echo -e "Test 8 completed."
+if [ -z "$1" ] || [ "8" = "$1" ]; then
+	echo -e "Compiling with -fstack-protector-strong, full relro, PIE, -fno-plt, and -fstack-check"
+	make clean
+	LDFLAGS='-Wl,-z,relro,-z,now -pie' CFLAGS='-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIE -fno-plt -fstack-check' make
+	echo -e "Compilation finished, running test 8"
+	bash -c './Run' &>> $results
+	echo -e "Test 8 completed."
+fi
